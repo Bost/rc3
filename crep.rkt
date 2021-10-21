51892d440c56d86bd718d7f9e3835ce1d755ca1f
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-10-21 14:10:37 +0200

    crep.rkt: accept multiple files

--- crep.rkt
+++ crep.rkt
@@ -1,51 +1,54 @@
 #lang racket
 
-(require "notes.rkt"
-         "notes-reader.rkt")
+(require
+ ;; (prefix-in com: "common.rkt")
+ "notes.rkt" ;; is used indeed
+ "notes-reader.rkt")
 
 (define pattern (make-parameter ""))
-(define files (make-parameter ""))
+(define files (make-parameter (list)))
 
 ;; Thanks to https://jackwarren.info/posts/guides/racket/racket-command-line/
 (define parser
   (command-line
    #:usage-help
-   "Search in note-file(s) for a pattern. Return note-block(s)"
+   "Search in note-file(s) for a pattern. Return note-block(s).
+E.g.:
+  racket crep.rkt -f ~/dev/notes/org-roam/20210805195404-guix.org -p title
 
+  racket crep.rkt -f ~/dev/notes/org-roam/20210805195404-guix.org \\
+                  -f ~/dev/notes/org-roam/20210729234518-racket.org \\
+                  -p title"
+   #:multi
+   [("-f" "--files") fs
+                     "File(s) to search through"
+                     (files
+                      ;; `files` was initialized to be '()
+                      (append (files) (list fs)))]
    #:once-each
-   [("-f" "--files") NAME
-                    "File(s) to search through"
-                    (files NAME)]
    [("-p" "--pattern") NAME
                     "Search pattern"
                     (pattern NAME)]
 
-   #:args () (void)))
+   ;; no other arguments are accepted
+   #;#;#;#:args () (void)))
 
-;; echo '#lang reader "notes-reader.rkt"' | cat - nds.org > nds.rkt
-
-(define t
-  "a b c
-
-a x c
-
-aa bb cc
-
-aaa bbb ccc
-")
-(define in
-  (open-input-file (files))
-  #;(open-input-string t))
-
-(define src #f)
-
-(define exp `(notes ,@(cons (pattern) (parse-notes src in))))
-#;exp
-;; the expression must be evaluated in a namespace.
-;; Thanks to https://stackoverflow.com/q/16266934 for a hint
+(define add-src-location-info #f)
 (define-namespace-anchor a)
-(define ns (namespace-anchor->namespace a))
-(printf "~a\n" (string-join (eval exp ns) "\n\n"))
 
-
-
+((compose
+  (curry printf "~a")
+  (lambda (strs) (string-join strs "\n"))
+  (curry map (lambda (f)
+               (define inf (open-input-file f) #;(open-input-string t))
+               (define exp
+                 `(notes ,@(cons (pattern)
+                                 (parse-notes add-src-location-info inf))))
+               ;; the expression must be evaluated in a namespace.
+               ;; Thanks to https://stackoverflow.com/q/16266934 for a hint
+               (define ns (namespace-anchor->namespace a))
+               (define strs (string-join (eval exp ns) "\n\n"))
+               #;(close-input-port inf)
+               (format "~a\n~a\n" f strs)))
+  (curry filter (curry string<? "")))
+ (files))
